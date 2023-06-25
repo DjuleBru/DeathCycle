66d7caac81f48bf4a95a28a45dca89906234dec0
DjuleBru <julien-taconet@hotmail.com>
2023-06-25 14:27:31 +0200

    Refactored

--- Assets/Scripts/LoopManager.cs
+++ Assets/Scripts/LoopManager.cs
@@ -3,110 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoopManager : MonoBehaviour {
-
-<<<<<<< Updated upstream
-    // Dictionaries relative to this loop's player data
-    private Dictionary<int, Vector3> playerPositionRecord = new Dictionary<int, Vector3>();
-    private Dictionary<int, bool> playerIsJump = new Dictionary<int, bool>();
-    private Dictionary<int, bool> playerIsAttack = new Dictionary<int, bool>();
-    private Dictionary<int, bool> playerIsSpecial = new Dictionary<int, bool>();
-    private Dictionary<int, bool> playerIsIdle = new Dictionary<int, bool>();
-
-    private Player player;
-    private Vector3 playerPosition;
-
-    private List<PlayerActions> playerActions = new List<PlayerActions>();
-    private List<PlayerActions> playerActionsPlayback = new List<PlayerActions>();
-
-    private float loopTime = 3f;
-    private float loopTimer = 0f;
-    private int loopIndex = 0;
-
-    private bool isRecording = true;
-    public bool IsRecording { get { return isRecording; } }
-
-    public void Start() {
-        player = GetComponent<Player>();
-    }
-
-    void Update() {
-        if (isRecording) {
-            Recording();
-        } else {
-            PlayBack();
-        }
-
-    }
-
-    void Recording() {
-        if (loopTimer <= loopTime) {
-
-            playerPosition = player.GetPlayerPosition();
-            playerPositionRecord.Add(loopIndex, playerPosition);
-
-            playerActions = player.GetPlayerActions();
-            HandlePlayerActionsRecord(loopIndex, playerActions);
-
-            loopTimer += Time.deltaTime;
-            loopIndex++;
-        } else {
-            isRecording = false;
-            loopTimer = 0;
-            loopIndex = 0;
-        }
-    }
-
-    void PlayBack() {
-        if (loopIndex < playerPositionRecord.Count) {
-
-            playerPosition = playerPositionRecord[loopIndex];
-            playerActionsPlayback = HandlePlayerActionsPlayBack(loopIndex);
-
-            player.SetPlayerposition(playerPosition);
-            player.HandleActions(playerActionsPlayback);
-            playerActionsPlayback.Clear();
-
-            loopTimer += Time.deltaTime;
-            loopIndex++;
-        }
-    }
-
-    void HandlePlayerActionsRecord(int loopIndex, List<PlayerActions> playerActions) {
-        foreach (PlayerActions playerAction in playerActions) {
-            if (playerAction == PlayerActions.attack) {
-                playerIsAttack.Add(loopIndex, true);
-            }
-            if (playerAction == PlayerActions.special) {
-                playerIsSpecial.Add(loopIndex, true);
-            }
-            if (playerAction == PlayerActions.jump) {
-                playerIsJump.Add(loopIndex, true);
-            }
-            if (playerAction == PlayerActions.idle) {
-                playerIsIdle.Add(loopIndex, true);
-            }
-        }
-    }
-
-    List<PlayerActions> HandlePlayerActionsPlayBack(int loopIndex) {
-        if(playerIsAttack.ContainsKey(loopIndex)) {
-            playerActionsPlayback.Add(PlayerActions.attack);
-            Debug.Log("PlayerActions.attack");
-        }
-        if (playerIsJump.ContainsKey(loopIndex)) {
-            playerActionsPlayback.Add(PlayerActions.jump);
-            Debug.Log("PlayerActions.jump");
-        }
-        if (playerIsSpecial.ContainsKey(loopIndex)) {
-            playerActionsPlayback.Add(PlayerActions.special);
-            Debug.Log("PlayerActions.Special");
-        }
-        if (playerIsIdle.ContainsKey(loopIndex)) {
-            playerActionsPlayback.Add(PlayerActions.idle);
-            Debug.Log("PlayerActions.Idle");
-        }
-        return playerActionsPlayback;
-=======
     public static LoopManager Instance { get; private set; }
 
     private float loopTime = 2f;
@@ -146,6 +42,5 @@ public class LoopManager : MonoBehaviour {
                 loopNumber++;
             }
         }
->>>>>>> Stashed changes
     }
 }
