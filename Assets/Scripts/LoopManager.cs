ad1e850c229d6b9650a19951993f549b54d4fdfd
DjuleBru <julien-taconet@hotmail.com>
2023-06-25 17:07:26 +0200

    Refactor LoopManager

--- Assets/Scripts/LoopManager.cs
+++ Assets/Scripts/LoopManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoopManager : MonoBehaviour {
+<<<<<<< Updated upstream
 
     // Dictionaries relative to this loop's player data
     private Dictionary<int, Vector3> playerPositionRecord = new Dictionary<int, Vector3>();
@@ -105,5 +106,47 @@ public class LoopManager : MonoBehaviour {
             Debug.Log("PlayerActions.Idle");
         }
         return playerActionsPlayback;
+=======
+    public static LoopManager Instance { get; private set; }
+
+    private float loopTime = 2f;
+    private float loopRecordingTimer = 0f;
+    private float loopPlaybackTimer = 0f;
+
+    private int loopNumber = 1;
+    private bool isRecording;
+
+    public bool IsRecording { get { return isRecording; } }
+    public int LoopNumber { get { return loopNumber; } }
+
+    private void Awake() {
+        if (Instance != null) {
+            Debug.LogError("There is more than one LoopManager Instance");
+        }
+        Instance = this;
+    }
+
+    void Update() {
+        Debug.Log(loopNumber);
+        // 1: recording
+        if (loopRecordingTimer <= loopTime) {
+            isRecording = true;
+            loopRecordingTimer += Time.deltaTime;
+        }
+        // 2: recording end
+        else {
+            // 3: playback 
+            if (loopPlaybackTimer <= loopTime) {
+                loopPlaybackTimer += Time.deltaTime;
+                isRecording = false;
+            } else {
+                // 4: playback end
+                loopRecordingTimer = 0;
+                loopPlaybackTimer = 0;
+                loopNumber++;
+                Debug.Log("Loop n " + loopNumber);
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
